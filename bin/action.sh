78a15696fc10d07832c54993b84a409cfc59a87a
Samuel Sjoberg <samuel.sjoberg@extendaretail.com>
2020-07-03 08:50:16 +0200

    feat: Add support for relaxed linting of historic commits (#4)

--- bin/action.sh
+++ bin/action.sh
@@ -4,17 +4,20 @@
 # We embed this in our image to avoid rebuilding it at action runtime
 
 usage() {
-  echo "Usage: $0 [-c <sha>] [-m <string>]" 1>&2
+  echo "Usage: $0 [-c <sha>] [-x <bool>] [-m <string>]" 1>&2
   exit 1
 }
 
-while getopts ":c:m:" opt; do
+while getopts ":c:m:x:" opt; do
   case "${opt}" in
     c)
       commit="$OPTARG"
       ;;
     m)
       message="$OPTARG"
+      ;;
+    x)
+      relaxed="$OPTARG"
       ;;
     *)
       usage
@@ -25,5 +28,16 @@ done
 if [ -n "$message" ]; then
   echo "$message" | exec /usr/local/bin/commitlint
 elif [ -n "$commit" ]; then
-  exec /usr/local/bin/commitlint --from "$commit"
+  if [ "$relaxed" == "true" ]; then
+    # Relaxed validation. Only check if this is a single commit.
+    if [ "$(git rev-parse "$commit")" == "$(git rev-parse HEAD)" ]; then
+      echo "commitlint HEAD"
+      exec /usr/local/bin/commitlint --from "$commit"
+    else
+      echo "commitlint relaxed, not linting $commit...HEAD"
+    fi
+  else
+    echo "commitlint $commit...HEAD"
+    exec /usr/local/bin/commitlint --from "$commit"
+  fi
 fi
